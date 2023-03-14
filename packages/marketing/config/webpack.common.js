a672267a8b6c9e63ec48b2c21b83f8257c4f4bda
Rajaguru <s.rajaguru.developer@gmail.com>
2023-03-14 16:14:31 +0530

    container deploy script added

--- packages/marketing/config/webpack.common.js
+++ packages/marketing/config/webpack.common.js
@@ -1,3 +1,6 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJSON = require('../package.json');
 module.exports = {
     module: {
         rules: [
@@ -13,5 +16,18 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes : {
+                './MarketingApp' : './src/bootstrap'
+            },
+            shared : packageJSON.dependencies
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        })
+    ]
 }
