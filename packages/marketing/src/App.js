767705e7534fa99dd8dfcfd5dd7493249af5a227
Rajaguru <s.rajaguru.developer@gmail.com>
2023-03-16 14:34:46 +0530

    issue fixes

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
-const generatClassName = createGenerateClassName({
-  productionPrefix:"marketing"
-})
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
 
 export default () => {
   return (
     <div>
-      <StylesProvider generateClassName={generatClassName}>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
