ba01e84b677c870c5ce1c6274bc911420612864f
Rajaguru <s.rajaguru.developer@gmail.com>
2023-03-16 14:23:13 +0530

    CSS collision issue fixed

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header'
-
+const generatClassName = createGenerateClassName({
+  productionPrefix:"container"
+})
 export default () => {
   return (
     <BrowserRouter>
-    <div>
-      <Header />
-      <MarketingApp />
-    </div>
+      <StylesProvider generateClassName={generatClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generatClassName = createGenerateClassName({
+  productionPrefix:"marketing"
+})
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generatClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
